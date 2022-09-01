3e8d50d3ba505d98e654677c5b695d9e03f359c8
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-31 19:09:48 -0500

    Created DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const resultsNav = document.getElementById("resultsNav");
+const favoritesNav = document.getElementById("favoritesNav");
+const imagesContainer = document.querySelector(".images-container");
+const saveConfirmed = document.querySelector(".save-confirmed");
+const loader = document.querySelector(".loader");
+
 // NASA API
 const count = 10;
 const apiKey = "DEMO_KEY";
@@ -5,11 +11,62 @@ const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${co
 
 let resultsArray = [];
 
+function updateDOM() {
+  resultsArray.forEach((result) => {
+    // CARD CONTAINER
+    const card = document.createElement("div");
+    card.classList.add("card");
+    // LINK
+    const link = document.createElement("a");
+    link.href = result.hdurl;
+    link.title = "View Full Image";
+    link.target = "_blank";
+    // IMAGE
+    const image = document.createElement("img");
+    image.src = result.url;
+    image.alt = "NASA Picture of the Day";
+    image.loading = "lazy";
+    image.classList.add("card-img-top");
+    // CARD BODY
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    // CARD TITLE
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = result.title;
+    // SAVE TEXT
+    const saveText = document.createElement("p");
+    saveText.classList.add("clickable");
+    saveText.textContent = "Add To Favorites";
+    // CARD TEXT
+    const cardText = document.createElement("p");
+    cardText.textContent = result.explanation;
+    // FOOTER CONTAINER
+    const footer = document.createElement("small");
+    footer.classList.add("text-muted");
+    // DATE
+    const date = document.createElement("strong");
+    date.textContent = result.date;
+    // COPYRIGHT
+    const copyrightResult =
+      result.copyright === undefined ? "" : result.copyright;
+    const copyright = document.createElement("span");
+    copyright.textContent = ` ${copyrightResult}`;
+    // APPEND
+    footer.append(date, copyright);
+    cardBody.append(cardTitle, saveText, cardText, footer);
+    link.appendChild(image);
+    card.append(link, cardBody);
+    imagesContainer.appendChild(card);
+  });
+}
+
 // GET 10 IMAGES FROM NASA API
 async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
+    updateDOM();
   } catch (error) {
     // CATCH ERROR HERE
   }
